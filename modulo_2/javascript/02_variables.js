0855d958ba674b6be6a40c4c98141a1b0a683925
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-21 21:55:25 -0500

    clase 2025-10-21

---
+++ modulo_2/javascript/02_variables.js
@@ -0,0 +1,36 @@
+console.log("VARIABLES");
+var saludo1="HOLA";
+var saludo2="HOLA DESDE UNA VARIABLE";
+console.log(saludo1);
+
+console.log(saludo1+" "+saludo2);
+console.log(`${saludo1} ${saludo2} ${4+5}`);
+let numero = 24;
+const angulo = 45;
+console.log(numero, angulo);
+
+console.log("TIPOS DE DATOS");
+var nombre="Darth Vader";
+console.log("String ", typeof(nombre));
+var tipoNumero=44444;
+console.log("Tipo Numerico ", typeof(tipoNumero));
+var tipoBooleano=true;
+console.log("Tipo Booleano ", typeof(tipoBooleano));
+var tipoIndefinido=undefined;
+console.log("Tipo Indefinido ", typeof(tipoIndefinido));
+var tipoNula=null;
+console.log("Tipo Nula ", typeof(tipoNula));
+var tipoSimbolo=Symbol("clave");
+console.log("Tipo Simbolo ", typeof(tipoSimbolo));
+var tipoBigEntero=432424n;
+console.log("Tipo Big Entero ", typeof(tipoBigEntero));
+
+
+const persona = {
+    nombre: "Carlos",
+    edad: 30
+}
+console.log("Tipo Objeto ", typeof(persona));
+console.log(pesona);
+console.log(persona.edad);
+console.log(persona.nombre);
